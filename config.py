29d33c07532d7ef58b91193003800b4109e1ac32
joetansey1 <70726791+joetansey1@users.noreply.github.com>
2025-05-19 00:13:40 -0700

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # config.py
 
 INFLUX_URL = "http://grafanapi.local:8086"  # Or 127.0.0.1 if using Pi directly
-INFLUX_TOKEN = "erfvQYLBhrERH9bu8ORZzBRbfQ4GuEM8_f4MYCvVeMJhe38F-K2hZf0C0aNj4MPhk-GyRxufLNRzNrAs0h8gjg=="
+INFLUX_TOKEN = "UR TOKEN HERE"
 ORG = "localorg"  # Replace with your actual InfluxDB org name
 BUCKET = "GT350"
